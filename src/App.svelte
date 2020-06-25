fe29bf09086283b8e12cb601913f89586e19b3e6
Hemal Ruparelia <hemal@192-168-1-106.tpgi.com.au>
2020-06-25 09:10:48 +0800

    Set up PouchDB and Tailwind

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import PouchDB from 'pouchdb-browser'
+
+	const Exercises = new PouchDB('exercises');
+
+	let name;
+	let units;
+</script>
+
+<main class="p-4">
+	<h1>Manage exercises</h1>
+	<input class="p-2 border border-gray-500 rounded" placeholder="Name" bind:value={name}/>	
+	<input class="p-2 border border-gray-500 rounded" placeholder="Units" bind:value={units}/>	
+	<button class="py-2 px-4 bg-green-500 shadow text-white rounded hover:bg-green-600">Add</button>
+</main>
